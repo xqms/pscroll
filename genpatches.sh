77eeb09f994669310f2906b6a498fc6923e8f825
Max Schwarz <Max@x-quadraht.de>
2010-11-07 14:30:42 +0100

    updated genpatches.sh script

--- genpatches.sh
+++ genpatches.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-git diff --relative=xserver-xorg-input-evdev-2.3.2/ 992d32c20d766936aa6e1756d767c3f95830c338..HEAD xserver-xorg-input-evdev-2.3.2/src/*.{c,h} xserver-xorg-input-evdev-2.3.2/include/*.h > xorg-input-evdev-pscroll.patch
-git diff --relative=xorg-server-1.7.6/ 9c37fd15597771efa575af878ee3d88e4ab3b7a0..HEAD xorg-server-1.7.6/ > xorg-server-no_integration.patch
-git diff --relative=xserver-xorg-input-synaptics-1.2.2/ e82c3b1a2a3b51fc0275dcb824f6ff41be083834..HEAD xserver-xorg-input-synaptics-1.2.2/ > xorg-input-synaptics-pscroll.patch
+
+git diff 858899e2b2da239f3ddf..$(git subtree split -P xf86-input-evdev 858899e2b2da239f3ddf..) > xf86-input-evdev.patch
+git diff 512c58176a34d9e47980..$(git subtree split -P xf86-input-synaptics 512c58176a34d9e47980..) > xf86-input-synaptics.patch
+git diff f993107c68111e74f5c4..$(git subtree split -P xserver f993107c68111e74f5c4..) > xserver.patch
+
+
